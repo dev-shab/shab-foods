db0a375985d3c0e7fae6ca1b07a3ede31ceafb9f
dev-shab <shabreeshnair1998@gmail.com>
2024-11-16 00:29:36 +0530

    food app, with mock restaurant data

--- App.js
+++ App.js
@@ -1,8 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./shab_foods.png";
+import restaurantData from "./restaurants.json";
 
-const App = () => <h1>Shab foods</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const { restaurantData } = props;
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    restaurantData;
+  return (
+    <div className="restaurant-card">
+      <img
+        className="restaurant-logo"
+        alt="restaurant-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString}</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        {restaurantData.restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurantData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+    <Body />
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<AppLayout />);
